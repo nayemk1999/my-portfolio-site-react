149eca89abef04a2b1fd775f7ee07e3128793ce6
Nayem <nayemk900@gmail.com>
2021-04-29 14:50:34 +0600

    Google maps Added

--- src/components/Home/GetInTouch/GetInTouch.js
+++ src/components/Home/GetInTouch/GetInTouch.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import './GetInTouch.css'
+import GoogleMaps from './GoogleMaps';
 
 const GetInTouch = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     return (
-        <div className='mt-5'>
+        <div className='mt-5 container p-5'>
             <h1 className=' text-white text-center text-uppercase text-shadow'>Get in touch</h1>
             <span className='divider'></span>
             <span className='divider2'></span>
-            <div className="row align-items-center">
-                <div className="col-md-6">
+            <div className="row align-items-center p-5">
+                <div className="col-md-6 form">
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <input defaultValue="" placeholder='Enter your name' {...register("name", { required: true })} /> <br/>
+                        <input className='form-control' id='name' defaultValue="" placeholder='Enter your name' {...register("name", { required: true })} /> <br/>
                         {errors.name && <span>This field is required</span>}
-                        <input placeholder='Enter your email' {...register("email", { required: true })} /><br/>
+                        <input className='form-control' placeholder='Enter your email' {...register("email", { required: true })} /><br/>
                         {errors.email && <span>This field is required</span>}
-                        <input type='text-area' placeholder='Enter your message' {...register("message", { required: true })} /><br/>
+                        <input className='form-control' type='text-area' placeholder='Enter your message' {...register("message", { required: true })} /><br/>
                         {errors.message && <span>This field is required</span>}
-                        <input type="submit" />
+                        <input className="btn btn-success" value='Send Message' type="submit" />
                     </form>
                 </div>
                 <div className="col-md-6">
-                    <h1>Map..............</h1>
+                    <GoogleMaps></GoogleMaps>
                 </div>
             </div>
         </div>
---
+++ src/components/Home/GetInTouch/GoogleMaps.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const location = {
+    lat: 23.797322,
+    lng: 90.423645
+};
+const onLoad = marker => {
+    console.log('marker: ', marker)
+}
+function GoogleMaps() {
+    return (
+        <LoadScript
+            googleMapsApiKey="AIzaSyCK2zpg38A5V2QDZmPtVhB4WArPTLia2LM"
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={location}
+                zoom={10}
+            >
+                <Marker
+                    onLoad={onLoad}
+                    position={location}
+                />
+            </GoogleMap>
+        </LoadScript>
+    )
+}
+
+export default React.memo(GoogleMaps)
