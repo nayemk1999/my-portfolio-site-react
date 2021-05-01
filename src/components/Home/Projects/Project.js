6cf4617377a2f6b102babc8e6dc4e7b1a1b849dd
Md. Nayem Khan <nayemk900@gmail.com>
2021-05-01 05:34:24 -0700

    project setup

---
+++ src/components/Home/Projects/Project.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Project = (props) => {
+    const { name, img1, github, live} = props.project;
+  return (
+    <div className='col-md-3'>
+      <div className="card p-3" >
+        <img className="img-fluid" src={img1} alt="project image" />
+        <div className="card-body">
+          <h6 className="card-text projectName">{name}</h6>
+          <div className='d-flex align-items-center justify-content-center project-link'>
+              <a href={github} className='p-2 m-1 btn-success rounded' target='_blank'>GitHub</a>
+              <a href={live} className='p-2 m-1 btn-success rounded' target='_blank'>Live</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Home/Projects/Projects.js
+++ src/components/Home/Projects/Projects.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import ParticlesContainer from '../../../ParticlesContainer/ParticlesContainer';
-import './Projects.css'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ParticlesContainer from "../../../ParticlesContainer/ParticlesContainer";
+import { projectsData } from "../../Resume/ResumeData";
+import Project from "./Project";
+import "./Projects.css";
 const Projects = () => {
-    return (
-            <div className='mt-5 container App'
-                style={{
-                    position: "relative",
-                    width: "100%",
-                    height: "100%"
-                }}
-            >
-                <ParticlesContainer />
-                <div
-                    style={{
-                        position: "relative",
-                        width: "100%",
-                        height: "100%"
-                    }}
-                >
-                    <h1 className=' text-white text-center text-uppercase text-shadow'>Projects</h1>
-                    <span className='divider'></span>
-                    <span className='divider2'></span>
-                    <Link to='/projects' className="btn btn-success btn-custom">Go To Projects</Link>
-                </div>
-            </div>
-    );
+  return (
+    <div
+      className="mt-5 container App"
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <ParticlesContainer />
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <h1 className=" text-white text-center text-uppercase text-shadow">
+          Projects
+        </h1>
+        <span className="divider"></span>
+        <span className="divider2"></span>
+        <div className="row gx-5">
+          {projectsData.map(project => <Project project={project}></Project>)
+          }
+        </div>
+
+        <Link to="/projects" className="btn btn-success btn-custom mt-3">
+          Go To Projects
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
