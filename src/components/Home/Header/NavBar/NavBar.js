d8ec0b75bb59efc69774a3ebc880e61ff2ee70b8
Nayem <nayemk900@gmail.com>
2021-05-04 16:07:22 +0600

    nabar edit

--- src/components/Home/Header/NavBar/NavBar.js
+++ src/components/Home/Header/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Logo from "../../../../image/portfolio-logo.png";
 import ParticlesContainer from "../../../../ParticlesContainer/ParticlesContainer";
@@ -14,36 +14,35 @@ const NavBar = () => {
       }}
     >
       <ParticlesContainer />
-      <div className='container'>
-        <Navbar className='navbar-menu' expand="lg sm">
-          <Navbar.Brand className='m-auto text-center' href="#home">
-            <img className="img-fluid w-75 " src={Logo} alt="" />
-          </Navbar.Brand>
-          <Navbar.Toggle className='ml-auto' style={{border: '1px solid black'}} aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className='text-center' id="basic-navbar-nav">
-            <Nav className="ml-auto ">
-              <Nav.Link>
-                <Link className='header-menu p-1' to="/home">Home</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link className='header-menu p-1' to="/about-me">About Me</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link className='header-menu p-1' to="/resume">Resume</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link className='header-menu p-1' to="/projects">Projects</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link className='header-menu p-1' to="/blogs">Blogs</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link className='header-menu p-1' to="/contact">Contact</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+      <Navbar className='navbar-menu container' expand="lg sm">
+        <Navbar.Brand className='m-auto text-center' >
+          <Link to='/'><img className="img-fluid w-75 " src={Logo} alt="" /></Link>
+        </Navbar.Brand>
+        <Navbar.Toggle className='ml-auto' style={{ border: '1px solid black' }} aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className='text-center ' id="basic-navbar-nav">
+          <Nav className="ml-auto ">
+            <Nav.Link>
+              <Link className='header-menu p-1' to="/home">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className='header-menu p-1' to="/about-me">About Me</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className='header-menu p-1' to="/resume">Resume</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className='header-menu p-1' to="/projects">Projects</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className='header-menu p-1' to="/blogs">Blogs</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className='header-menu p-1' to="/contact">Contact</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
     </section>
   );
 };
